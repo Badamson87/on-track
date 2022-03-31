d56830a31f9fe27e472df799bbc52e71b30156c4
Brian Adamson <brianadamson87@gmail.com>
2022-03-31 14:49:45 -0600

    Create Aunthenticate component

---
+++ src/components/Authenticate.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="Authenticate">
+    <h3 v-if="!isLoggedIn" class="Authenticate__Item" @click="toggleLoggedIn">Login</h3>
+    <h3 v-if="!isLoggedIn" class="Authenticate__Item" @click="toggleLoggedIn">Sign Up</h3>
+    <h3 v-else class="Authenticate__Item" @click="toggleLoggedIn">Sign Out</h3>
+  </section>
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-class-component";
+
+export default class Authenticate extends Vue {
+  _loggedIn = false;
+
+  toggleLoggedIn(): void {
+    this._loggedIn = !this._loggedIn
+  }
+
+  get isLoggedIn(): boolean {
+    return this._loggedIn;
+  }
+}
+
+</script>
+
+<style scoped lang="scss">
+.Authenticate {
+  display: flex;
+}
+.Authenticate__Item {
+  padding-right: .5rem;
+  padding-left: .5rem;
+}
+.Authenticate__Item:last-of-type {
+  color: red;
+}
+</style>
