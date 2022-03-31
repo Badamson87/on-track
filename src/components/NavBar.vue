50c142d4eea9edb8c17bd105bf724329d9b97187
Brian Adamson <brianadamson87@gmail.com>
2022-03-31 11:40:34 -0600

    Initial commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,13 @@
+<template>
+  <section>Nav Bar</section>
+</template>
+
+<script lang="ts">
+
+
+import { Vue } from "vue-class-component";
+
+export default class NavBar extends Vue {
+
+}
+</script>
