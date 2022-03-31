1baaed037c415bbc0291f5adb16ffea799cc10ab
Brian Adamson <brianadamson87@gmail.com>
2022-03-31 13:55:04 -0600

    Playing with scss and navbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
   <nav-bar/>
+<!--  <div id="nav">-->
+<!--    <router-link to="/">Home</router-link> |-->
+<!--    <router-link to="/about">About</router-link>-->
+<!--  </div>-->
   <router-view />
 </template>
 
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,16 @@
 <template>
-  <section>Nav Bar</section>
+  <section class="NavBar__Header">
+<!--    TODO replace this with logo-->
+    <h3 class="NavBar__Header__Title">On-Track</h3>
+<!--    TODO make this stick in the middle and stop moving-->
+    <h3 class="NavBar__Header__Title">Welcome To Church</h3>
+<!--    TODO make this its own component-->
+    <div class="NavBar__Header__AuthButtons">
+      <h3 v-if="!isLoggedIn" class="NavBar__Header__Item" @click="toggleLoggedIn">Login</h3>
+      <h3 v-if="!isLoggedIn" class="NavBar__Header__Item" @click="toggleLoggedIn">Sign Up</h3>
+      <h3 v-else class="NavBar__Header__Item" @click="toggleLoggedIn">Sign Out</h3>
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
@@ -8,6 +19,33 @@
 import { Vue } from "vue-class-component";
 
 export default class NavBar extends Vue {
+  _loggedIn = false;
+
+  toggleLoggedIn(): void {
+    this._loggedIn = !this._loggedIn
+  }
+
+  get isLoggedIn(): boolean {
+    return this._loggedIn;
+  }
+
 
 }
 </script>
+
+<style scoped lang="scss">
+.NavBar__Header {
+  display: flex;
+  justify-content: space-between;
+}
+.NavBar__Header__AuthButtons {
+  display: flex;
+}
+.NavBar__Header__Item {
+  padding-right: .5rem;
+  padding-left: .5rem;
+}
+//.NavBar__Header__Item:last-of-type {
+//  color: red;
+//}
+</style>
